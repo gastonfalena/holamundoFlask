c98b0d7a30cd1eefda3784823563bd406163be5f
gastonfalena <gastonfalena@gmail.com>
2024-03-17 17:36:34 -0300

    Add insert db and redirecting

--- holamundo.py
+++ holamundo.py
@@ -1,6 +1,19 @@
 from flask import Flask,request,url_for,redirect,abort,render_template
+from decouple import config
+import mysql.connector
 
 app =  Flask(__name__)
+
+MYSQL_HOST , MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB = config('MYSQL_HOST'),config('MYSQL_USER'), \
+config('MYSQL_PASSWORD'),config('MYSQL_DB')
+
+midb = mysql.connector.connect(
+   host = MYSQL_HOST,
+   user = MYSQL_USER,
+   password = MYSQL_PASSWORD,
+   database = MYSQL_DB
+)
+cursor = midb.cursor(dictionary=True)
 
 @app.route('/')
 def index():
@@ -37,3 +50,24 @@ def json():
 @app.route('/home',methods=['GET'])
 def home():
     return render_template('home.html',mensaje='Hola Mundo')
+
+@app.route('/db',methods=['GET'])
+def db():
+    cursor.execute('select * from Usuario')
+    usuarios = cursor.fetchall()
+    return  render_template('hola.html',usuarios=usuarios)
+
+
+@app.route('/crear',methods=['GET','POST'])
+def crear():
+    if request.method=="POST":
+        username = request.form['username']
+        email = request.form['email']
+        sql="insert into Usuario(username,email) values(%s,%s)"
+        #utilizamos value para que en username no puedan hacer sql injection
+        values=(username,email) 
+        #la funcion se encarga de concatenar los nombres correctamente
+        cursor.execute(sql,values)
+        midb.commit()
+        return redirect(url_for('db'))
+    return render_template('crear.html') 
